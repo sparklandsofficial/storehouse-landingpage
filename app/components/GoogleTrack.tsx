384513f04b98675b1e1e461bba73661b1f5d98a5
intheblackworld <intheblackworld@gmail.com>
2024-11-17 14:58:31 +0800

    modify

---
+++ app/components/GoogleTrack.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Script from 'next/script'
+
+export default function FacebookPixel() {
+  return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-528010636" />
+      <Script id="google-track">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-528010636');
+        `}
+      </Script>
+    </>
+  )
+}
