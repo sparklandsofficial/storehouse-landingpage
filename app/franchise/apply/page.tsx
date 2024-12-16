9b6800fa45db32ad4d4a94423091a4f55e4f8d5e
intheblackworld <intheblackworld@gmail.com>
2024-12-16 19:52:33 +0800

    add is_bink_card_bind verify

--- app/franchise/apply/page.tsx
+++ app/franchise/apply/page.tsx
@@ -2,7 +2,13 @@ export default function FranchiseApply() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-[#483729] mb-6">我要加盟</h1>
-      {/* 加入加盟申請表單 */}
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSc4hrxHETcHrgwI8-cS4Tvu6i1WteClhGa6eTyK3wfkxl4Kyg/viewform?usp=sf_link"
+        target="_blank"
+        className="bg-[#FF9E18] text-[#fff] px-8 py-3 rounded-full font-bold hover:bg-orange-400 transition-colors duration-300 inline-block"
+      >
+        填寫表單
+      </a>
     </div>
   );
-} 
+}
