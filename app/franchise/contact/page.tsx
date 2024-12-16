ba8efe2be4e2f3128003133fa4243d2a6f140932
intheblackworld <intheblackworld@gmail.com>
2024-12-16 11:41:02 +0800

    modify

--- app/franchise/contact/page.tsx
+++ app/franchise/contact/page.tsx
@@ -1,24 +1,43 @@
 export default function FranchiseContact() {
   return (
     <div className="container mx-auto px-4 py-8 max-md:p-0">
-      <h1 className="text-3xl font-bold text-[#483729] mb-12 text-center">加盟聯繫</h1>
-      
+      <h1 className="text-3xl font-bold text-[#483729] mb-12 text-center">
+        加盟聯繫
+      </h1>
+
       <div className="bg-[#FF9E18] p-12 rounded-lg max-w-3xl mx-auto text-center">
-        <h2 className="text-3xl font-bold text-white mb-8">全國加盟專線</h2>
+        <h2 className="text-3xl font-bold text-white mb-8">全台加盟專線</h2>
         <div className="text-xl space-y-6">
-          <p className="flex items-center justify-center space-x-3 max-md:flex-col">
-            <a 
-              href="tel:02-2796-3030" 
-              className="text-white text-3xl font-bold hover:text-gray-200 transition-colors duration-300 underline max-md:mb-2"
+          <p className="flex items-center justify-start space-x-3 max-md:flex-col">
+            <a
+              href="tel:02-2796-3030"
+              className="text-white text-2xl font-bold hover:text-gray-100 transition-colors duration-300 underline max-md:mb-2"
             >
               02-2796-3030
             </a>
-            <span className="text-white text-2xl">
-              (加盟專員廖先生)
-            </span>
+            <span className="text-white text-xl">(加盟專員廖先生)</span>
+          </p>
+          <p className="flex items-center justify-start space-x-3 max-md:flex-col">
+            <span className="text-white text-xl">Email:</span>
+            <a
+              href="mailto:avisl@sparklands.co"
+              className="text-white text-2xl font-bold hover:text-gray-100 transition-colors duration-300 underline max-md:mb-2"
+            >
+              avisl@sparklands.co
+            </a>
+          </p>
+          <p className="flex items-center justify-start space-x-3 max-md:flex-col">
+            <span className="text-white text-xl">Line 官方:</span>
+            <a
+              href="https://lin.ee/Z58WSDK"
+              target="_blank"
+              className="text-white text-2xl font-bold hover:text-gray-100 transition-colors duration-300 underline max-md:mb-2"
+            >
+              @125negus
+            </a>
           </p>
         </div>
       </div>
     </div>
   );
-} 
+}
--- app/franchise/process/page.tsx
+++ app/franchise/process/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 export default function FranchiseProcess() {
 
 
@@ -6,7 +7,7 @@ export default function FranchiseProcess() {
       <h1 className="text-3xl font-bold text-[#483729] mb-12 max-md:mb-4">加盟流程</h1>
       
       <div className="grid grid-cols-1 gap-6">
-        <img src="https://hd7872mjgdymyoov.public.blob.vercel-storage.com/%E6%88%AA%E5%9C%96%202024-12-13%20%E4%B8%8B%E5%8D%882.06.20-asYFXEjKov9FFZuKuAiINBPnUwn7rl.png" alt="" />
+        <Image className="w-full" src="/images/process.png" alt="加盟流程" width={779} height={1067} />
       </div>
     </div>
   );
