0c6d11c8aa4b5fb450b79616f1eb201da1f4fd34
intheblackworld <intheblackworld@gmail.com>
2024-12-17 12:21:15 +0800

    modify

--- app/franchise/apply/page.tsx
+++ app/franchise/apply/page.tsx
@@ -2,13 +2,7 @@ export default function FranchiseApply() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-[#483729] mb-6">我要加盟</h1>
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSc4hrxHETcHrgwI8-cS4Tvu6i1WteClhGa6eTyK3wfkxl4Kyg/viewform?usp=sf_link"
-        target="_blank"
-        className="bg-[#FF9E18] text-[#fff] px-8 py-3 rounded-full font-bold hover:bg-orange-400 transition-colors duration-300 inline-block"
-      >
-        填寫表單
-      </a>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc4hrxHETcHrgwI8-cS4Tvu6i1WteClhGa6eTyK3wfkxl4Kyg/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">載入中…</iframe>
     </div>
   );
-}
+} 
--- app/franchise/process/page.tsx
+++ app/franchise/process/page.tsx
@@ -7,7 +7,7 @@ export default function FranchiseProcess() {
       <h1 className="text-3xl font-bold text-[#483729] mb-12 max-md:mb-4">加盟流程</h1>
       
       <div className="grid grid-cols-1 gap-6">
-        <Image className="w-full" src="/images/process.png" alt="加盟流程" width={779} height={1067} />
+        <Image className="" src="/images/process.png" alt="加盟流程" width={779} height={1067} />
       </div>
     </div>
   );
