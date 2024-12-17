c117cdef3e4705333a51a1590fe9f34b6274f91e
intheblackworld <intheblackworld@gmail.com>
2024-12-17 12:30:57 +0800

    modify

--- app/franchise/apply/page.tsx
+++ app/franchise/apply/page.tsx
@@ -2,7 +2,7 @@ export default function FranchiseApply() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-[#483729] mb-6">我要加盟</h1>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc4hrxHETcHrgwI8-cS4Tvu6i1WteClhGa6eTyK3wfkxl4Kyg/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">載入中…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc4hrxHETcHrgwI8-cS4Tvu6i1WteClhGa6eTyK3wfkxl4Kyg/viewform?embedded=true" width="100%" height="700">載入中…</iframe>
     </div>
   );
 } 
