c2349613ed07ce3fdc08bf0e6cc62881a5cb3bc7
alexkwong01 <alexk@sparklands.co>
2026-08-25 18:55:18 +0800

    gamescope_app上架

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/app/gamescope/support",
+        destination: "/app/gamescope/support.html",
+      },
+      {
+        source: "/app/gamescope/privacy",
+        destination: "/app/gamescope/privacy.html",
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
